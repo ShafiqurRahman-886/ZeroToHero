85ae30d9e5ebbccf6c66e118ac63bc649b6af100
shafiq0920 <shafiq0920@gmail.com>
2021-09-19 02:36:17 -0400

    Chapter 1

---
+++ Chapter_1.py
@@ -0,0 +1,48 @@
+#PrintING Hello World
+
+
+print('Shafiqur Rahman')
+
+print ('Try This')
+
+print ('this line')
+print ('also this line')
+
+# Calculation In Python
+
+print (2+2)
+print (2+2*8/4)
+
+print (12/4)
+
+
+
+# 2/6 or 6/5 it alwyas return a float value means Decimal
+
+print (0.24)
+
+print (4./5)
+print (2*7.0)
+
+# Exponentiaiton in Python
+# Double astrek x**y in python means x to the power y
+
+print (4**2.)
+print (8**8)
+
+print (2**3**2)  #we can also do this
+print (2**9)
+
+print (9 **(1./2))
+
+
+# // will find only integer value of Divison aka quotient
+
+print (20//6)
+
+# % this represent remainder
+
+print (20%6)
+
+
+print(0.01**2**30)
